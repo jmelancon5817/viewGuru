6845a33e7bd82052493c8649eb6325e1b57505c2
Jacob Melancon <jmelancon5817@bellsouth.net>
2023-01-25 19:59:12 -0600

    Improve Optimization Feature

--- src/components/optimize.jsx
+++ src/components/optimize.jsx
@@ -1,28 +1,75 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Gallery } from "react-grid-gallery";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const Optimize = () => {
-  const [input, setInput] = useState("");
+  const location = useLocation();
+  const selectedVideo = location.state.selectedVideo;
+
+  const [currentThumbnail, setCurrentThumbnail] = useState(
+    selectedVideo.snippet.thumbnails.medium.url
+  );
+  const [alternateThumbnails, setAlternateThumbnails] = useState([]);
+  const [updateThumbnail, setUpdateThumbnail] = useState(false);
 
   const handleChange = (e) => {
-    setInput(e.target.value);
+    const files = e.target.files;
+    const thumbnails = [...alternateThumbnails];
+    for (let i = 0; i < files.length; i++) {
+      thumbnails.push(URL.createObjectURL(files[i]));
+    }
+    setAlternateThumbnails(thumbnails);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // optimization logic here
+  const handleUpdate = () => {
+    setUpdateThumbnail(true);
   };
+
+  const handleCancel = () => {
+    // Stop Optimization
+  };
+
+  useEffect(() => {
+    if (updateThumbnail) {
+      // Use the YouTube API to track view count of the video over time
+      // and determine which alternate thumbnail to use
+    }
+  }, [updateThumbnail]);
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Enter video URL or ID"
-          value={input}
-          onChange={handleChange}
+    <div className="optimize-container">
+      <h1>Initialize Optimization</h1>
+      <div className="current">
+        <p>Current Thumbnail:</p>
+        <img
+          src={currentThumbnail}
+          alt={selectedVideo.snippet.title}
+          className="current-thumbnail"
         />
-        <button type="submit">Optimize</button>
-      </form>
+      </div>
+      <div>
+        <p className="alternate-heading">Alternate Thumbnails:</p>
+        <div className="optimize-gallery">
+          <input
+            type="file"
+            multiple
+            onChange={handleChange}
+            className="optimize-input"
+          />
+          <Gallery
+            images={alternateThumbnails.map((thumbnail) => ({
+              src: thumbnail,
+            }))}
+            margin={12}
+            rowHeight={300}
+          />
+        </div>
+      </div>
+      <div className="optimize-buttons">
+        <button onClick={handleUpdate}>Submit</button>
+        <button onClick={handleCancel}>Cancel</button>
+      </div>
     </div>
   );
 };
