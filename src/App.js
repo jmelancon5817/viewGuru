400fb731ec426e023407f56ba2b7cb2b3525afe9
Jacob Melancon <jmelancon5817@bellsouth.net>
2023-02-20 10:10:59 -0600

    Modify Login State Structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import LandingPage from "./components/landingPage";
+import Dashboard from "./components/dashboard";
+import Optimize from "./components/optimize";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    if (localStorage.getItem("user")) {
+      setIsLoggedIn(true);
+    }
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route
+            path="/landingPage"
+            element={<LandingPage setIsLoggedIn={setIsLoggedIn} />}
+          />
+          <Route
+            path="dashboard"
+            element={<Dashboard setIsLoggedIn={setIsLoggedIn} />}
+          />
+          <Route path="optimize" element={<Optimize />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
